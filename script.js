da30b2247c68a19e5b8d1c4a8f81d2ddd3c8dab6
Deepanshu-Sharma-52 <166294671+Deepanshu-Sharma-52@users.noreply.github.com>
2024-08-11 07:01:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,15 +16,4 @@ document.getElementById('reservationForm').addEventListener('submit', function(e
     
     document.getElementById('output').innerHTML = output;
 
-    // Send email
-    emailjs.send("service_m1fngzj", "template_2u40f9j", {
-        from: from,
-        to: to,
-        date: date,
-        seats: seats
-    }).then(function(response) {
-        console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-        console.log('FAILED...', error);
-    });
-});
+   
